a47fbdf1784ef6e3481c4d2131a4b7a1c423d9cb
HJ_Kim <ruby45342@gmail.com>
2022-11-09 15:40:06 +0900

    security set

--- src/main/java/com/spring/board/config/WebContextConfig.java
+++ src/main/java/com/spring/board/config/WebContextConfig.java
@@ -18,27 +18,28 @@ public class WebContextConfig implements WebApplicationInitializer {
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		AnnotationConfigWebApplicationContext servletAppContext = new AnnotationConfigWebApplicationContext();
 		// set root servlet for use all servlets & filters
-		servletAppContext.register(ServletContextConfig.class);
-		// set dispacher servlet
+		servletAppContext.register(new Class[] { WebSecurityConfig.class, ServletContextConfig.class });
+//		servletAppContext.register();
+		// set dispacher servlet1
 		DispatcherServlet dispatcherServlet = new DispatcherServlet(servletAppContext);
 		ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", dispatcherServlet);
-		 // 부가 설정
+		// 부가 설정
 		servlet.setLoadOnStartup(1); // set first start servlet
 		servlet.addMapping("/"); // basic route
-		
+
 		// set root config file for connect db
 		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
 		rootContext.register(RootContextConfig.class);
-		
+
 		// set srping container
 		ContextLoaderListener listener = new ContextLoaderListener(rootContext);
-        servletContext.addListener(listener);
-        
-        // for character encoding for utf-8
-        FilterRegistration encoding = servletContext.addFilter("CharacterEncodingFilter", CharacterEncodingFilter.class);
-        encoding.setInitParameter("encoding", "UTF-8");
-        encoding.setInitParameter("forceEncoding", "true");
-        encoding.addMappingForUrlPatterns(null, true, "/*");
+		servletContext.addListener(listener);
+
+		// for character encoding for utf-8
+		FilterRegistration encoding = servletContext.addFilter("CharacterEncodingFilter", CharacterEncodingFilter.class);
+		encoding.setInitParameter("encoding", "UTF-8");
+		encoding.setInitParameter("forceEncoding", "true");
+		encoding.addMappingForUrlPatterns(null, true, "/*");
 	}
 
 }
--- src/main/java/com/spring/board/config/WebSecurityConfig.java
+++ src/main/java/com/spring/board/config/WebSecurityConfig.java
@@ -1,26 +1,44 @@
 package com.spring.board.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**");// Spring Security should completely ignore URLs starting with
+													// /resources/
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/", "/api/**", "/login").permitAll()
-				.antMatchers("/api/edit/**").authenticated(); // 일반사용자 접근 가능
-		// 인증 필요시 로그인 페이지와 로그인 성공시 리다이랙팅 경로 지정
-		http.formLogin().loginPage("/login").defaultSuccessUrl("/", true);
-		// 로그인이 수행될 uri 매핑 (post 요청이 기본)
-		http.formLogin().loginProcessingUrl("/login").defaultSuccessUrl("/", true);
-		// 인증된 사용자이지만 인가되지 않은 경로에 접근시 리다이랙팅 시킬 uri 지정
-		http.exceptionHandling().accessDeniedPage("/forbidden");
-		// logout
-		http.logout().logoutUrl("/logout").logoutSuccessUrl("/");
+//		http.csrf().disable();
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin()// Form 로그인 인증 기능이 작동함
+				.defaultSuccessUrl("/**", true).permitAll().and().logout();
+//		http.authorizeRequests().antMatchers("/login").permitAll()
+//				.antMatchers("/", "/api/one/**").hasRole("USER").anyRequest().authenticated(); // 일반사용자 접근 가능
+//		// 인증 필요시 로그인 페이지와 로그인 성공시 리다이랙팅 경로 지정
+//		http.formLogin().loginPage("/login").defaultSuccessUrl("/", true);
+//		// 로그인이 수행될 uri 매핑 (post 요청이 기본)
+//		http.formLogin().loginProcessingUrl("/login").defaultSuccessUrl("/", true);
+//		// 인증된 사용자이지만 인가되지 않은 경로에 접근시 리다이랙팅 시킬 uri 지정
+//		http.exceptionHandling().accessDeniedPage("/forbidden");
+//		// logout
+//		http.logout().logoutUrl("/logout").logoutSuccessUrl("/");
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// basic user & guest setting
+		auth.inMemoryAuthentication().withUser("user").password("{noop}user").roles("USER");
+		auth.inMemoryAuthentication().withUser("guest").password("{noop}guest").roles("GUEST");
 	}
 }
